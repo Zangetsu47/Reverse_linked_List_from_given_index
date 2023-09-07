3445fc938b4bfa9a1f7a71e55ff681ff756e6136
Zangetsu47 <77004587+Zangetsu47@users.noreply.github.com>
2023-09-07 14:39:40 +0530

    Update reverse.java
    function to reverse the Linked List within a given range

--- reverse.java
+++ reverse.java
@@ -1,1 +1,44 @@
-
+/*
+class Node{
+  int data;
+  int next;
+  Node(int data)
+  {
+    this.data=data;
+    this.next=null;
+  }
+}
+class ListNode{
+ ListNode head;
+ ListnNode()
+ {
+   head=null;
+ }
+}
+*/
+void remove(int start,int end)
+{
+  Node current=head;
+  if(head==null)
+    return;
+  int i=1;
+  List<Integer>list=new LinkedList<>();
+  while(current!=null)
+    {
+      if(i>=start && i<=end)
+      {
+        list.add(0,current.data);
+      }
+      i++;
+      current=current.next;
+    }
+  current=head;
+  i=1;int k=0;
+  while(current!=null)
+    {
+      if(i>=start && i<=end)
+        current.data=list.get(k++);
+      i++;
+      current=current.next;
+    }
+}
